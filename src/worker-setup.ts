0b068b371699982775305384507cda0e7101c522
David Miranda Grinstein <davidmirandagrinstein@gmail.com>
2020-12-03 12:58:15 -0300

    Delete worker-setup.ts

--- src/worker-setup.ts
+++
@@ -1,5 +0,0 @@
-export default (worker: Function): SharedWorker => {
-    const code = worker.toString();
-    const blob = new Blob(["(" + code + ")()"]);
-    return new SharedWorker(URL.createObjectURL(blob));
-}
